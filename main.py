68389d79a2cf31d9eb034a6459717b5b3c1f8385
DrinkH20 <89287648+DrinkH20@users.noreply.github.com>
2022-01-13 16:30:53 -0800

    I added the gun and perfect rotation. And also the bullets come out at the same place no matter the velocity.

--- main.py
+++ main.py
@@ -8,56 +8,62 @@ player_img = resize_img(pygame.image.load("Player.png"), p_width, p_height)
 player_mask = pygame.mask.from_surface(player_img)
 level_img = resize_img(pygame.image.load("Level1.png"), 2000, 600)
 level_mask = pygame.mask.from_surface(level_img)
-bullet_img = resize_img(pygame.image.load("Bullets.png"), 10, 10)
+bullet_img = resize_img(pygame.image.load("Bullets.png"), 5, 5)
 bullet_mask = pygame.mask.from_surface(bullet_img)
+current_gun = resize_img(pygame.image.load("Weapon 1.png"), 45, 45)
 run = True
-Playerx = 200
-Playery = 200
+Plyr_X = 200
+Plyr_Y = 200
+VelX = 0
+VelY = 0
 bullet_list = []
 time_count = 0
 scroll_x = 0
 scroll_y = 0
+bullet_wait = 5
 
 
 class Player:
     def __init__(self):
         self.img = player_img
         self.mask = player_mask
-        self.x = Playerx
-        self.y = Playery
-        self.xvel = 0
-        self.yvel = 0
+        self.x = Plyr_X
+        self.y = Plyr_Y
+        self.VelX = 0
+        self.VelY = 0
 
     def move_left(self):
-        self.xvel -= 1
+        self.VelX -= 1
 
     def move_right(self):
-        self.xvel += 1
+        self.VelX += 1
 
     def jump(self):
-        self.yvel = -10
+        self.VelY = -10
 
-    def movex(self):
-        self.xvel = (self.xvel * .9)
-        self.x += self.xvel
+    def move_x(self):
+        self.VelX = (self.VelX * .9)
+        self.x += self.VelX
 
-    def movey(self):
-        self.yvel += .5
-        self.y += self.yvel
+    def move_y(self):
+        self.VelY += .1
+        self.y += self.VelY
 
-    def collidex(self):
-        self.x += self.xvel * -1
-        self.xvel = 0
+    def collide_x(self):
+        self.x += self.VelX * -1
+        self.VelX = 0
 
-    def collidey(self):
-        self.y += self.yvel * -1
-        self.yvel = 0
+    def collide_y(self):
+        self.y += self.VelY * -1
+        self.VelY = 0
 
     def set_vars(self):
-        global Playerx
-        global Playery
-        Playerx = self.x
-        Playery = self.y
+        global Plyr_X, Plyr_Y
+        global VelX, VelY
+        Plyr_X = self.x
+        Plyr_Y = self.y
+        VelX = self.VelX
+        VelY = self.VelY
 
     def draw_player(self, window):
         window.blit(self.img, (self.x + scroll_x, self.y + scroll_y))
@@ -71,27 +77,48 @@ class Player:
 
 class Bullets:
     def __init__(self):
-        global Playerx, Playery
+        global Plyr_X, Plyr_Y
         global mx, my
-        self.bltx = Playerx + 5
-        self.blty = Playery + 5
-        self.dir = math.atan2(Playerx - mx + scroll_x, Playery - my + scroll_y)
+        self.X = Plyr_X
+        self.Y = Plyr_Y
+        self.dir = math.atan2(Plyr_X - mx + scroll_x, Plyr_Y - my + scroll_y)
         self.vx = math.sin(self.dir) * -1
         self.vy = math.cos(self.dir) * -1
         self.img = bullet_img
         self.delete = 0
+        self.rotated = 0
+        self.place = 0
 
     def move(self):
-        self.bltx += self.vx * 10
-        self.blty += self.vy * 10
+        self.X += self.vx * 10
+        self.Y += self.vy * 10
         self.delete += 1
         return self.delete
 
     def draw(self):
-        WIN.blit(self.img, (self.bltx + scroll_x, self.blty + scroll_y))
+        if self.delete > 2:
+            self.rotated = pygame.transform.rotate(self.img, self.dir * (180 / 3.14))
+            self.place = self.X-int(self.rotated.get_width()/2)+10, self.Y-int(self.rotated.get_height() / 2)+15
+            WIN.blit(self.rotated, (self.place[0] + scroll_x + (VelX * 2), self.place[1] + scroll_y + (VelY * 2)))
+
+
+class Gun:
+    def __init__(self):
+        self.weapon = current_gun
+        self.dir = 0
+        self.place = 0
+        self.rotated = 0
+
+    def draw_gun(self):
+        self.weapon = current_gun
+        self.dir = math.atan2(Plyr_X - mx + scroll_x, Plyr_Y - my + scroll_y)
+        self.rotated = pygame.transform.rotate(self.weapon, self.dir * (180 / 3.14))
+        self.place = Plyr_X - int(self.rotated.get_width() / 2) + 10, Plyr_Y - int(self.rotated.get_height() / 2) + 15
+        WIN.blit(self.rotated, (self.place[0] + scroll_x, self.place[1] + scroll_y))
 
 
 Player = Player()
+Gun = Gun()
 
 while run:
     keys = pygame.key.get_pressed()
@@ -103,18 +130,18 @@ while run:
 
     Player.draw_player(WIN)
 
-    Player.movey()
+    Player.move_y()
 
     if Player.colliding():
-        Player.collidey()
+        Player.collide_y()
         if Player.colliding(1):
             if keys[pygame.K_UP]:
                 Player.jump()
 
-    Player.movex()
+    Player.move_x()
 
     if Player.colliding():
-        Player.collidex()
+        Player.collide_x()
 
     if keys[pygame.K_LEFT]:
         Player.move_left()
@@ -123,7 +150,7 @@ while run:
 
     Player.set_vars()
 
-    if click[0] and time_count > 10:
+    if click[0] and time_count > bullet_wait:
         time_count = 0
         bullet_list.append(())
         bullet_list[len(bullet_list) - 1] = Bullets()
@@ -134,10 +161,12 @@ while run:
         if bullet.move() >= 100:
             bullet_list.remove(i)
 
+    Gun.draw_gun()
+
     time_count += 1
 
-    scroll_x -= (Playerx + scroll_x - 250) * .1
-    scroll_y -= (Playery + scroll_y - 250) * .1
+    scroll_x -= (Plyr_X + scroll_x - 250) * .1
+    scroll_y -= (Plyr_Y + scroll_y - 250) * .1
 
     pygame.display.update()
     for event in pygame.event.get():
